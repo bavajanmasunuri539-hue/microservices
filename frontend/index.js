41473a006ab7e623a0872d680cfd5044d7b7b7be
BAVAJAN MASUNURI <bavajanmasunuri539@gmail.com>
2026-07-30 12:26:36 +0530

    Updated frontend message

--- frontend/index.js
+++ frontend/index.js
@@ -4,7 +4,7 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, {
         "Content-Type": "text/plain"
     });
-    res.end("Frontend Microservice");
+    res.end("Frontend Microservice Version 2");
 });
 
 server.listen(3000, () => {
